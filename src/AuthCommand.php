98af1737463c1226dbc1da4bbddc2daaa0fc43a1
Jefter.Rafael <jefterrafael1.1@gmail.com>
2023-01-08 20:03:43 -0300

    ...

--- src/AuthCommand.php
+++ src/AuthCommand.php
@@ -185,18 +185,18 @@ class AuthCommand extends Command
         if (file_exists(resource_path('js/Views/auth/login.vue'))) {
             unlink(resource_path('js/views/auth/login.vue'));
             copy(
-                __DIR__ . './vue-stubs/Views/auth/login.vue',
+                __DIR__ . '/vue-stubs/Views/auth/login.vue',
                 resource_path('js/Views/auth/login.vue')
             );
         } elseif (!file_exists(resource_path('js/Views/auth'))) {
             mkdir(resource_path('js/Views/auth'), 0755, true);
              copy(
-                __DIR__ . './vue-stubs/Views/auth/login.vue',
+                __DIR__ . '/vue-stubs/Views/auth/login.vue',
                 resource_path('js/Views/auth/login.vue')
             );
         } else {
              copy(
-                __DIR__ . './vue-stubs/Views/auth/login.vue',
+                __DIR__ . '/vue-stubs/Views/auth/login.vue',
                 resource_path('js/Views/auth/login.vue')
             );
         }
